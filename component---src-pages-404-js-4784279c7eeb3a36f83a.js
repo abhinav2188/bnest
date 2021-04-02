1f214a50a1bce24734d16a56ceafeb034a0e2962
abhinav2188 <abhinav.mca19.du@gmail.com>
2021-04-02 20:22:37 +0530

    Updates

---
+++ component---src-pages-404-js-4784279c7eeb3a36f83a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,l){"use strict";l.r(t);var a=l(7294);t.default=function(e){return a.createElement("div",{className:"py-24"},a.createElement("p",null,"© BioNest Eco Solutions"),a.createElement("p",{className:"text-4xl text-green-500"},"404 Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4784279c7eeb3a36f83a.js.map
